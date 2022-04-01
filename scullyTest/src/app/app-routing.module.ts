9d06649a376a7cbb79741c1feadeb1fc53aa11f7
Yarushin Sergey <s_yarushin@gcore.lu>
2022-04-01 13:51:35 +0300

    fixed some styles and links

--- scullyTest/src/app/app-routing.module.ts
+++ scullyTest/src/app/app-routing.module.ts
@@ -4,17 +4,19 @@ import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
   {
-    path: 'test',
-    component: HomeComponent
-  }
-  ,
-  {
     path: 'users',
     loadChildren:  () => import('./users/users.module').then(m => m.UsersModule)
   },
   {
-    path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)
-  }];
+    path: 'blog',
+    loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
